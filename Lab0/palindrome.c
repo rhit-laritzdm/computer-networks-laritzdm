ddd903cc1da36eb2e2625cc49c5e29ac1774893a
rhit-laritzdm <laritzdm@rose-hulman.edu>
2024-03-14 16:48:41 -0400

    worked on palindrome

--- Lab0/palindrome.c
+++ Lab0/palindrome.c
@@ -10,6 +10,7 @@
 /*Optional functions, uncomment the next two lines
  * if you want to create these functions after main: */
 bool readLine(char** line, size_t* size, size_t* length) {
+    *length = getline(line, size, stdin);
     return true;
 }
 
@@ -27,20 +28,47 @@ bool readLine(char** line, size_t* size, size_t* length) {
 */
 
 bool isPalindrome(const char* line, size_t len) {
-
+    if (isalpha(line[i])) { // check if line is alphabetic
+        return false;
+    }
+    // 0:R 1:A 2:C 3:E 4:C 5:A 6:R 7: \n
+    for (int i = 0; i < len; i++) {
+        if ()
+    }
+    return false;
 }
 
-int main(int argc, char *argv[]){
-    //your code here
-    if (isalpha(line[i])) { // check if line is alphabetic
-        /*code*/
+int main(int argc, char *argv[]) {
+    char *line;
+    size_t size;
+    size_t length;
+
+    while (1) {
+        printf("Type a word: ");
+        readLine(&line, &size, &length);
+        length = getline(&line, &size, stdin);
+        printf("%zu characters were read\n", length);
+        printf("%zu is the size\n", size);
+        printf("You said: %s\n", line);
+
+        if (isPalindrome(line, length)) {
+            printf("\"%s\" is a palindrome", word);
+        } else {
+            printf("\"%s\" is not a palindrome", word);
+        }
+        free(line);
     }
-    while (readLine(line, size, length)) {
+    //while (1) {
+
+        /*
+        readLine(&line, &size, &length)
         if (isPalindrome(line, len)) {
             printf("\"%s\" is a palindrome", word);
         } else {
             printf("\"%s\" is not a palindrome", word);
         }
-    }
+         */
+    //}
+    // Cleanup
     return 0;
 }
